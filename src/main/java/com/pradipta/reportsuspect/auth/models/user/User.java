d2a8901b29b4f58b8a443c67ce225729df9500a0
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-03-28 13:50:03 +0530

    login and signup 2

--- src/main/java/com/pradipta/reportsuspect/auth/models/user/User.java
+++ src/main/java/com/pradipta/reportsuspect/auth/models/user/User.java
@@ -4,6 +4,8 @@ import com.pradipta.reportsuspect.auth.models.role.Role;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -11,6 +13,7 @@ import javax.persistence.ManyToOne;
 @Data
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstname;
     private String lastname;

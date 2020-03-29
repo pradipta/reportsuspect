cd6f832a940c59cbf375de149d6209d013a02b30
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-03-29 18:01:34 +0530

    Stocksmore (#12)
    * stocks api added
    
    * stocks api written
    
    Co-authored-by: Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>

--- src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockHandler.java
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockHandler.java
@@ -1,6 +1,12 @@
 package com.pradipta.reportsuspect.apis.stock.model.stock;
 
 import com.pradipta.reportsuspect.apis.stock.dto.StockDto;
+import com.pradipta.reportsuspect.apis.stock.model.hospital.Hospital;
+import com.pradipta.reportsuspect.apis.stock.model.hospital.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +14,9 @@ import org.springframework.stereotype.Component;
 public class StockHandler {
     @Autowired
     private StockService stockService;
+
+    @Autowired
+    private HospitalService hospitalService;
 
     public Stock addStockByStockDto(StockDto stockDto) {
         Stock stock = stockFromStockDto(stockDto);
@@ -17,5 +26,13 @@ public class StockHandler {
     private Stock stockFromStockDto(StockDto stockDto) {
         //TODO pradipta
         return new Stock();
+    }
+
+    public Stock getStockById(Integer id) {
+        Optional<Hospital> hospital = hospitalService.getHospitalById(id);
+        if (!hospital.isPresent()) {
+            return null;
+        }
+        return stockService.findStockByHospital(hospital.get()).orElse(null);
     }
 }

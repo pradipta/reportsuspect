c7c89521079e01e4cffb970fdd7c5a7e1998a5b7
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-03-29 17:59:19 +0530

    stocks api written

--- src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockController.java
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockController.java
@@ -1,10 +1,29 @@
 package com.pradipta.reportsuspect.apis.stock.model.stock;
 
+import com.pradipta.reportsuspect.apis.stock.dto.StockDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+    @Autowired
+    private StockHandler stockHandler;
 
+    @PostMapping("/addNewStock")
+    private Stock addStock(@RequestBody StockDto stockDto) {
+        return stockHandler.addStockByStockDto(stockDto);
+    }
+
+    @GetMapping("/getStockByHospital")
+    private Stock getStock(@PathVariable("hospitalid") Integer hospitalid) {
+        return stockHandler.getStockById(hospitalid);
+    }
+
+    //TODO PUT apis for Stock
 }
--- src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockHandler.java
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockHandler.java
@@ -1,13 +1,19 @@
 package com.pradipta.reportsuspect.apis.stock.model.stock;
 
 import com.pradipta.reportsuspect.apis.stock.dto.StockDto;
+import com.pradipta.reportsuspect.apis.stock.model.hospital.Hospital;
+import com.pradipta.reportsuspect.apis.stock.model.hospital.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class StockHandler {
     @Autowired
     private StockService stockService;
+    @Autowired
+    private HospitalService hospitalService;
 
     public Stock addStockByStockDto(StockDto stockDto) {
         Stock stock = stockFromStockDto(stockDto);
@@ -17,5 +23,13 @@ public class StockHandler {
     private Stock stockFromStockDto(StockDto stockDto) {
         //TODO pradipta
         return new Stock();
+    }
+
+    public Stock getStockById(Integer id) {
+        Optional<Hospital> hospital = hospitalService.getHospitalById(id);
+        if (!hospital.isPresent()) {
+            return null;
+        }
+        return stockService.findStockByHospital(hospital.get()).orElse(null);
     }
 }

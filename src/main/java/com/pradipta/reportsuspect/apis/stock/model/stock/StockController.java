30981bdd795a6dbfb75127f88b1364f6217c9415
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-03-29 16:19:45 +0530

    stocks api added

---
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockController.java
@@ -0,0 +1,10 @@
+package com.pradipta.reportsuspect.apis.stock.model.stock;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+}

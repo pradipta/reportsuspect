db29dda691748215c136e281dd93fcc392642fe3
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-04-01 00:31:36 +0530

    Changed ajax call

--- src/main/java/com/pradipta/reportsuspect/apis/stock/controller/StockController.java
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/controller/StockController.java
@@ -4,12 +4,14 @@ import com.pradipta.reportsuspect.apis.stock.dto.StockDto;
 import com.pradipta.reportsuspect.apis.stock.model.stock.Stock;
 import com.pradipta.reportsuspect.apis.stock.model.stock.StockHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/stock")
@@ -17,12 +19,19 @@ public class StockController {
     @Autowired
     private StockHandler stockHandler;
 
+    @GetMapping("/update")
+    public ModelAndView showForm(Authentication authentication) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("stockadd");
+        return modelAndView;
+    }
+
     @PostMapping("/{hospitalId}/updateStock")
     private Stock updateStock(@RequestBody StockDto stockDto, @PathVariable("hospitalId") Integer hospitalId) {
         return stockHandler.updateStockByStockDto(stockDto, hospitalId);
     }
 
-    @GetMapping("/getStockByHospital")
+    @GetMapping("/getStockByHospital/{hospitalid}")
     private Stock getStock(@PathVariable("hospitalid") Integer hospitalid) {
         return stockHandler.getStockByHospitalId(hospitalid);
     }

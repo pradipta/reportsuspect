d6243abc6198a7c358c532cc8fc04d548cb7ffb6
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-03-31 23:55:09 +0530

    Contacttracingfrontendintegration (#22)
    * integration 1
    
    * Added stockAdd page
    
    * Edited patient.html
    
    * Added stockAdd | Modified hospital template
    
    * Added ajax call in hospital template
    
    * Add hospital flow success
    
    * stock fucked up
    
    * fixed the fuck up
    
    Co-authored-by: Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
    Co-authored-by: nasirAhmedPR <nasir.ahmed@parkingrhino.com>

--- src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockService.java
+++ src/main/java/com/pradipta/reportsuspect/apis/stock/model/stock/StockService.java
@@ -1,11 +1,9 @@
 package com.pradipta.reportsuspect.apis.stock.model.stock;
 
-import com.pradipta.reportsuspect.apis.stock.dto.StockDto;
 import com.pradipta.reportsuspect.apis.stock.model.hospital.Hospital;
 import com.pradipta.reportsuspect.apis.stock.model.hospital.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
@@ -18,16 +16,18 @@ public class StockService {
         return stockRepository.save(stock);
     }
 
-    public Stock updateStock(StockDto stockDto, Integer hospitalId) {
+    public Stock updateStock(Stock stock, Integer hospitalId) {
         Optional<Hospital> hospitalOptional = hospitalService.getHospitalById(hospitalId);
         if (!hospitalOptional.isPresent()) {
             return null;
         }
-        Stock stock = hospitalOptional.get().getStock();
-        stock.setVaccines(stockDto.getVaccines());
-        stock.setMasks(stockDto.getMasks());
-        stock.setBeds(stockDto.getBeds());
-        stock.setVentilators(stockDto.getVentilators());
+        if (hospitalOptional.get().getStock() == null) {
+            Stock generatedStock = stockRepository.save(stock);
+            Hospital hospital = hospitalOptional.get();
+            hospital.setStock(generatedStock);
+            hospitalService.addHospital(hospital);
+        }
+        stock.setId(hospitalOptional.get().getStock().getId());
         return stockRepository.save(stock);
     }
 }
